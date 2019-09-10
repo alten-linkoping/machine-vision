3a03c1a04cfd4c09f6b4d6390d81e156996017ff
Emma Riedl <emma.riedl@hotmail.se>
2019-09-10 14:00:23 +0200

    Ready to test with client

--- main.cpp
+++ main.cpp
@@ -16,25 +16,14 @@ int main(int argc, char const *argv[])
 	cv::glob("MOT17-09-FRCNN/img1/*.jpg", fn, false);
 
 	std::vector<cv::Mat> images;
+	std::vector<std::string> imageNames;
 	size_t count = fn.size(); //number of jpg files in images folder
 	for (size_t i=0; i<count; i++)
-    	images.push_back(cv::imread(fn[i]));
-
-	/*
-	motionDetection *MD = new motionDetection(images);
-	MD->LKTracker();
-	MD->setupTracker("MEDIANFLOW");
-	std::cout << "It works" << std::endl;
-	std::cout << "Images read: " << MD->getImages().size() << std::endl;
-	
-	namedWindow( "Display window", cv::WINDOW_AUTOSIZE );// Create a window for display.
-	for (int i = 0; i < MD->getImages().size(); ++i)
 	{
-		    imshow( "Display window", MD->getImages()[i]);                   // Show our image inside it.
-
-    		cv::waitKey(25); 
+    	images.push_back(cv::imread(fn[i]));
+    	imageNames.push_back(std::string(fn.erase(fn.begin(), fn.end()-8)));
 	}
-          */                                // Wait for a keystroke in the window
+
 
     opticalFlow *OF = new opticalFlow(images);
     OF->calculateFlow();
--- opticalFlow.cpp
+++ opticalFlow.cpp
@@ -3,9 +3,10 @@
 #include "opticalFlow.h"
 
 
-opticalFlow::opticalFlow(std::vector<cv::Mat> loadedImages)
+opticalFlow::opticalFlow(std::vector<cv::Mat> loadedImages, std::vector<std::string> loadedImageNames)
 {
 	this->images = loadedImages;
+	this->imageNames = loadedImageNames;
 				
 }
 
@@ -13,12 +14,12 @@ opticalFlow::opticalFlow(std::vector<cv::Mat> loadedImages)
 void opticalFlow::detectEdges()
 {
 	/// Detect edges using Threshold
-			cv::Mat img_thresh = cv::Mat::zeros(this->frame.rows, this->frame.cols, CV_8UC1);
+			cv::Mat img_thresh = cv::Mat::zeros(this->images[this->currentFrame].rows, this->images[this->currentFrame].cols, CV_8UC1);
 			cv::threshold(this->gray_bgr, img_thresh, 245, 255, cv::THRESH_BINARY_INV);
 			cv::dilate(img_thresh, img_thresh, 0, cv::Point(-1, -1), 30);
 			cv::erode(img_thresh, img_thresh, 0, cv::Point(-1, -1), 30);
 			cv::dilate(img_thresh, img_thresh, 0, cv::Point(-1, -1), 30);
-			imshow("tresh",img_thresh);
+			//imshow("tresh",img_thresh);
 			cv::findContours(img_thresh, this->contours, this->hierarchy, cv::RETR_EXTERNAL, cv::CHAIN_APPROX_SIMPLE);
 			for (auto i = 0; i < this->contours.size(); i++)
 			{
@@ -26,28 +27,28 @@ void opticalFlow::detectEdges()
 				cv::approxPolyDP(cv::Mat(this->contours[i]), contours_poly[i], 3, true);
 				cv::Rect box = cv::boundingRect(cv::Mat(contours_poly[i]));
 				if (box.width > 50 && box.height > 50 && box.width < 800 && box.height < 900) {
-					rectangle(this->frame,
+					rectangle(this->images[this->currentFrame],
 						box.tl(), box.br(),
 						cv::Scalar(0, 255, 0), 4);
-					//this->boxes.append([box.x, box.y, box.height, box.width]);
+						this->boxes.push_back(std::vector<int> {box.x, box.y, box.height, box.width});
 				}
 			
 			}
 	
 
 			/// Show in a window
-			cv::namedWindow("Contours", CV_WINDOW_AUTOSIZE);
-			imshow("Contours", this->frame);
+			//cv::namedWindow("Contours", CV_WINDOW_AUTOSIZE);
+			//imshow("Contours", this->images[this->currentFrame]);
 }
 
 void opticalFlow::calculateFlow()
 {
-	cv::namedWindow("flow", cv::WINDOW_AUTOSIZE);
+	//cv::namedWindow("flow", cv::WINDOW_AUTOSIZE);
 
 	for (auto i = 0; i < this->images.size(); ++i)
 	{
-		this->frame = this->images[i];
-		cv::cvtColor(this->frame, this->gray, cv::COLOR_BGR2GRAY);
+		//this->frame = this->images[i];
+		cv::cvtColor(this->images[this->currentFrame], this->gray, cv::COLOR_BGR2GRAY);
 
 
 		if (!prevgray.empty())
@@ -61,17 +62,20 @@ void opticalFlow::calculateFlow()
 			drawOptFlowMap(0.5, 16, CV_RGB(0, 255, 0));
 			//imshow("flow", this->cflow);
 			this->drawHsv();
-			this->gray_bgr = cv::Mat::zeros(this->frame.rows, this->frame.cols, CV_8UC1);
+			this->gray_bgr = cv::Mat::zeros(this->images[this->currentFrame].rows, this->images[this->currentFrame].cols, CV_8UC1);
 			cv::cvtColor(this->img_bgr, this->gray_bgr, CV_BGR2GRAY);
 			cv::normalize(this->gray_bgr, this->gray_bgr, 0, 255, cv::NORM_MINMAX, CV_8UC1);
 			cv::blur(this->gray_bgr, this->gray_bgr, cv::Size(5, 5));
-			imshow("gray", this->gray_bgr);
+			//imshow("gray", this->gray_bgr);
 
 			this->detectEdges();
+			this->addToJson();
 
 		}
 		cv::waitKey(5); 
 		std::swap(this->prevgray, this->gray);
+		this->currentFrame++;
+		this->boxes.erase(boxes.begin(), boxes.end());
 	}
 	
 }
@@ -121,23 +125,34 @@ void opticalFlow::drawOptFlowMap(double scale, int step, const cv::Scalar& color
 
 void opticalFlow::createJsonObj(const v8::FunctionCallbackInfo<v8::Value> &args)
 {
-	v8::Isolate *isolate = args.GetIsolate();
+	this->isolate = args.GetIsolate();
 	this->obj = v8::Object::New(isolate);
 }
 
 void opticalFlow::addToJson()
 {
 
+	v8::Local<v8::Array> array = v8::Array::New(this->isolate, 4);
+
+	for (int i = 0; i < this->boxes.size(); ++i)
+	{
+		
+		array->Set(0, v8::Integer::New(this->isolate, boxes[i][0]));
+      	array->Set(1, v8::Integer::New(this->isolate, boxes[i][1]));
+      	array->Set(2, v8::Integer::New(this->isolate, boxes[i][2]));
+      	array->Set(3, v8::Integer::New(this->isolate, boxes[i][3]));
+
+		this->obj->Set(v8::String::NewFromUtf8(this->isolate, this->imageNames[currentFrame].c_str()), array);
+	}
+
 }
 
 void opticalFlow::sendJson(const v8::FunctionCallbackInfo<v8::Value> &args)
 {
-	v8::Isolate *isolate = args.GetIsolate();
+	//auto total = v8::Number::New(this->isolate, 2);
+	//args.GetReturnValue().Set(total);
 
-	auto total = v8::Number::New(isolate, 2);
-	args.GetReturnValue().Set(total);
-
-	//args.GetReturnValue().Set(this->obj);
+	args.GetReturnValue().Set(this->obj);
 }
 
 
