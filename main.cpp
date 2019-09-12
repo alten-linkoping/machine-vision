1d644de328ba71229520d8b8193de85792c1c81b
Emma Riedl <emma.riedl@hotmail.se>
2019-09-12 14:29:47 +0200

    gyp working

--- main.cpp
+++ main.cpp
@@ -3,33 +3,163 @@
 #include "opticalFlow.h"
 #include <iostream>
 #include <string>
+#include <napi.h>
 
-void init(v8::Local<v8::Object> exports, auto Method)
+std::string hello()
+{
+	// code 
+	std::vector<cv::String> fn;
+	cv::glob("MOT17-09-FRCNN/img1/00000*.jpg", fn, false);
+
+	opticalFlow OF;
+	std::vector<std::vector<int> > tempBoxCoords;
+	std::string imageBoxCoordinates;
+	imageBoxCoordinates.append("{");
+
+	// Load and calculate on the first frame
+	OF.setFrame(cv::imread(fn[0]));
+	OF.calculateFlow();
+
+
+	for (size_t i=1; i<fn.size(); i++)
 	{
-  		NODE_SET_METHOD(exports, "opticalFlow", Method);
+		OF.setFrame(cv::imread(fn[i]));
+		OF.calculateFlow();
+
+		tempBoxCoords = OF.getBoxes();
+		std::string tempName = fn[i];
+	
+		imageBoxCoordinates.append("\"" + tempName.erase(0,20) + "\"");
+		imageBoxCoordinates.append(":{");
+    	
+    	for (int j = 0; j < tempBoxCoords.size(); j++)
+   		{
+   			
+	     	imageBoxCoordinates.append("\"");
+	     	imageBoxCoordinates.append("Box" + std::to_string(j+1));
+	     	imageBoxCoordinates.append("\"");
+	     	imageBoxCoordinates.append(":");
+	     	imageBoxCoordinates.append("\"");
+	     	std::string coord = "";
+	     	coord = std::to_string(tempBoxCoords[j][0]) + "," + std::to_string(tempBoxCoords[j][1]) + "," + std::to_string(tempBoxCoords[j][2]) + "," + std::to_string(tempBoxCoords[j][3]);
+	     	imageBoxCoordinates.append(coord);
+	     	imageBoxCoordinates.append("\"");
+
+	     	if (j != tempBoxCoords.size()-1)
+	     	{
+	      	 	imageBoxCoordinates.append(",");
+	     	}
+	     	else
+	     	{
+	     		imageBoxCoordinates.append("}");
+	     	}
+   		}
+
+
+		if (i == fn.size()-1)
+		{
+			imageBoxCoordinates.append("}");
+		}
+		else
+		{
+			imageBoxCoordinates.append(",");
+		}
+
+		
 	}
+	
+    return imageBoxCoordinates;
+}
+
+
+
+Napi::String HelloWrapped(const Napi::CallbackInfo& info) 
+{
+  Napi::Env env = info.Env();
+  Napi::String returnValue = Napi::String::New(env, hello());
+  
+  return returnValue;
+}
+
+Napi::Object Init(Napi::Env env, Napi::Object exports) 
+{
+  exports.Set(
+"hello", Napi::Function::New(env, HelloWrapped)
+  );
+ 
+  return exports;
+}
+
+
 
 int main(int argc, char const *argv[])
 {
-	/* code */
+	// code 
 	std::vector<cv::String> fn;
-	cv::glob("MOT17-09-FRCNN/img1/*.jpg", fn, false);
+	cv::glob("MOT17-09-FRCNN/img1/00000*.jpg", fn, false);
 
-	std::vector<cv::Mat> images;
-	std::vector<std::string> imageNames;
-	size_t count = fn.size(); //number of jpg files in images folder
-	for (size_t i=0; i<count; i++)
+	opticalFlow OF;
+	std::vector<std::vector<int> > tempBoxCoords;
+	std::string imageBoxCoordinates;
+	imageBoxCoordinates.append("{");
+
+	// Load and calculate on the first frame
+	OF.setFrame(cv::imread(fn[0]));
+	OF.calculateFlow();
+
+
+	for (size_t i=1; i<fn.size(); i++)
 	{
-    	images.push_back(cv::imread(fn[i]));
-    	imageNames.push_back(std::string(fn.erase(fn.begin(), fn.end()-8)));
+		OF.setFrame(cv::imread(fn[i]));
+		OF.calculateFlow();
+
+		tempBoxCoords = OF.getBoxes();
+		std::string tempName = fn[i];
+	
+		imageBoxCoordinates.append("\"" + tempName.erase(0,20) + "\"");
+		imageBoxCoordinates.append(":{");
+    	
+    	for (int j = 0; j < tempBoxCoords.size(); j++)
+   		{
+   			
+	     	imageBoxCoordinates.append("\"");
+	     	imageBoxCoordinates.append("Box" + std::to_string(j+1));
+	     	imageBoxCoordinates.append("\"");
+	     	imageBoxCoordinates.append(":");
+	     	imageBoxCoordinates.append("\"");
+	     	std::string coord = "";
+	     	coord = std::to_string(tempBoxCoords[j][0]) + "," + std::to_string(tempBoxCoords[j][1]) + "," + std::to_string(tempBoxCoords[j][2]) + "," + std::to_string(tempBoxCoords[j][3]);
+	     	imageBoxCoordinates.append(coord);
+	     	imageBoxCoordinates.append("\"");
+
+	     	if (j != tempBoxCoords.size()-1)
+	     	{
+	      	 	imageBoxCoordinates.append(",");
+	     	}
+	     	else
+	     	{
+	     		imageBoxCoordinates.append("}");
+	     	}
+   		}
+
+
+		if (i == fn.size()-1)
+		{
+			imageBoxCoordinates.append("}");
+		}
+		else
+		{
+			imageBoxCoordinates.append(",");
+		}
+
+		
 	}
 
-
-    opticalFlow *OF = new opticalFlow(images);
-    OF->calculateFlow();
-
-	NODE_MODULE(opticalFlow_addon, init(OF->sendJson()));
-
+	std::cout << imageBoxCoordinates << std::endl;
+	
     return 0;
-   
 }
+
+NODE_API_MODULE(opticalFlow_addon, Init)
+
+
--- opticalFlow.cpp
+++ opticalFlow.cpp
@@ -2,19 +2,20 @@
 
 #include "opticalFlow.h"
 
-
-opticalFlow::opticalFlow(std::vector<cv::Mat> loadedImages, std::vector<std::string> loadedImageNames)
+void opticalFlow::setFrame(cv::Mat frame)
 {
-	this->images = loadedImages;
-	this->imageNames = loadedImageNames;
-				
+	this->frame = frame;
 }
 
+std::vector<std::vector<int> > opticalFlow::getBoxes()
+{
+	return this->boxes;
+}
 
 void opticalFlow::detectEdges()
 {
 	/// Detect edges using Threshold
-			cv::Mat img_thresh = cv::Mat::zeros(this->images[this->currentFrame].rows, this->images[this->currentFrame].cols, CV_8UC1);
+			cv::Mat img_thresh = cv::Mat::zeros(this->frame.rows, this->frame.cols, CV_8UC1);
 			cv::threshold(this->gray_bgr, img_thresh, 245, 255, cv::THRESH_BINARY_INV);
 			cv::dilate(img_thresh, img_thresh, 0, cv::Point(-1, -1), 30);
 			cv::erode(img_thresh, img_thresh, 0, cv::Point(-1, -1), 30);
@@ -27,7 +28,7 @@ void opticalFlow::detectEdges()
 				cv::approxPolyDP(cv::Mat(this->contours[i]), contours_poly[i], 3, true);
 				cv::Rect box = cv::boundingRect(cv::Mat(contours_poly[i]));
 				if (box.width > 50 && box.height > 50 && box.width < 800 && box.height < 900) {
-					rectangle(this->images[this->currentFrame],
+					rectangle(this->frame,
 						box.tl(), box.br(),
 						cv::Scalar(0, 255, 0), 4);
 						this->boxes.push_back(std::vector<int> {box.x, box.y, box.height, box.width});
@@ -38,17 +39,15 @@ void opticalFlow::detectEdges()
 
 			/// Show in a window
 			//cv::namedWindow("Contours", CV_WINDOW_AUTOSIZE);
-			//imshow("Contours", this->images[this->currentFrame]);
+			//imshow("Contours", this->frame);
 }
 
 void opticalFlow::calculateFlow()
 {
+	this->boxes.clear();
 	//cv::namedWindow("flow", cv::WINDOW_AUTOSIZE);
 
-	for (auto i = 0; i < this->images.size(); ++i)
-	{
-		//this->frame = this->images[i];
-		cv::cvtColor(this->images[this->currentFrame], this->gray, cv::COLOR_BGR2GRAY);
+		cv::cvtColor(this->frame, this->gray, cv::COLOR_BGR2GRAY);
 
 
 		if (!prevgray.empty())
@@ -62,23 +61,20 @@ void opticalFlow::calculateFlow()
 			drawOptFlowMap(0.5, 16, CV_RGB(0, 255, 0));
 			//imshow("flow", this->cflow);
 			this->drawHsv();
-			this->gray_bgr = cv::Mat::zeros(this->images[this->currentFrame].rows, this->images[this->currentFrame].cols, CV_8UC1);
+			this->gray_bgr = cv::Mat::zeros(this->frame.rows, this->frame.cols, CV_8UC1);
 			cv::cvtColor(this->img_bgr, this->gray_bgr, CV_BGR2GRAY);
 			cv::normalize(this->gray_bgr, this->gray_bgr, 0, 255, cv::NORM_MINMAX, CV_8UC1);
 			cv::blur(this->gray_bgr, this->gray_bgr, cv::Size(5, 5));
 			//imshow("gray", this->gray_bgr);
 
 			this->detectEdges();
-			this->addToJson();
 
 		}
-		cv::waitKey(5); 
+		//cv::waitKey(5); 
 		std::swap(this->prevgray, this->gray);
-		this->currentFrame++;
-		this->boxes.erase(boxes.begin(), boxes.end());
 	}
 	
-}
+
 
 void opticalFlow::drawHsv() {
 	//extract x and y channels
@@ -122,39 +118,5 @@ void opticalFlow::drawOptFlowMap(double scale, int step, const cv::Scalar& color
 			cv::circle(this->cflow, cv::Point(x, y), 2, color, -1);
 		}
 }
-
-void opticalFlow::createJsonObj(const v8::FunctionCallbackInfo<v8::Value> &args)
-{
-	this->isolate = args.GetIsolate();
-	this->obj = v8::Object::New(isolate);
-}
-
-void opticalFlow::addToJson()
-{
-
-	v8::Local<v8::Array> array = v8::Array::New(this->isolate, 4);
-
-	for (int i = 0; i < this->boxes.size(); ++i)
-	{
-		
-		array->Set(0, v8::Integer::New(this->isolate, boxes[i][0]));
-      	array->Set(1, v8::Integer::New(this->isolate, boxes[i][1]));
-      	array->Set(2, v8::Integer::New(this->isolate, boxes[i][2]));
-      	array->Set(3, v8::Integer::New(this->isolate, boxes[i][3]));
-
-		this->obj->Set(v8::String::NewFromUtf8(this->isolate, this->imageNames[currentFrame].c_str()), array);
-	}
-
-}
-
-void opticalFlow::sendJson(const v8::FunctionCallbackInfo<v8::Value> &args)
-{
-	//auto total = v8::Number::New(this->isolate, 2);
-	//args.GetReturnValue().Set(total);
-
-	args.GetReturnValue().Set(this->obj);
-}
-
-
 
 
