8d7f26592bb7b6f59afd0a982f5ddde5da0d2ae9
artch932 <artch932@sliu.se>
2019-09-13 13:50:22 +0200

    This is the last revert...
    This reverts commit 1d644de328ba71229520d8b8193de85792c1c81b.

--- opticalFlow.cpp
+++ opticalFlow.cpp
@@ -2,20 +2,19 @@
 
 #include "opticalFlow.h"
 
-void opticalFlow::setFrame(cv::Mat frame)
+
+opticalFlow::opticalFlow(std::vector<cv::Mat> loadedImages, std::vector<std::string> loadedImageNames)
 {
-	this->frame = frame;
+	this->images = loadedImages;
+	this->imageNames = loadedImageNames;
+				
 }
 
-std::vector<std::vector<int> > opticalFlow::getBoxes()
-{
-	return this->boxes;
-}
 
 void opticalFlow::detectEdges()
 {
 	/// Detect edges using Threshold
-			cv::Mat img_thresh = cv::Mat::zeros(this->frame.rows, this->frame.cols, CV_8UC1);
+			cv::Mat img_thresh = cv::Mat::zeros(this->images[this->currentFrame].rows, this->images[this->currentFrame].cols, CV_8UC1);
 			cv::threshold(this->gray_bgr, img_thresh, 245, 255, cv::THRESH_BINARY_INV);
 			cv::dilate(img_thresh, img_thresh, 0, cv::Point(-1, -1), 30);
 			cv::erode(img_thresh, img_thresh, 0, cv::Point(-1, -1), 30);
@@ -28,7 +27,7 @@ void opticalFlow::detectEdges()
 				cv::approxPolyDP(cv::Mat(this->contours[i]), contours_poly[i], 3, true);
 				cv::Rect box = cv::boundingRect(cv::Mat(contours_poly[i]));
 				if (box.width > 50 && box.height > 50 && box.width < 800 && box.height < 900) {
-					rectangle(this->frame,
+					rectangle(this->images[this->currentFrame],
 						box.tl(), box.br(),
 						cv::Scalar(0, 255, 0), 4);
 						this->boxes.push_back(std::vector<int> {box.x, box.y, box.height, box.width});
@@ -39,15 +38,17 @@ void opticalFlow::detectEdges()
 
 			/// Show in a window
 			//cv::namedWindow("Contours", CV_WINDOW_AUTOSIZE);
-			//imshow("Contours", this->frame);
+			//imshow("Contours", this->images[this->currentFrame]);
 }
 
 void opticalFlow::calculateFlow()
 {
-	this->boxes.clear();
 	//cv::namedWindow("flow", cv::WINDOW_AUTOSIZE);
 
-		cv::cvtColor(this->frame, this->gray, cv::COLOR_BGR2GRAY);
+	for (auto i = 0; i < this->images.size(); ++i)
+	{
+		//this->frame = this->images[i];
+		cv::cvtColor(this->images[this->currentFrame], this->gray, cv::COLOR_BGR2GRAY);
 
 
 		if (!prevgray.empty())
@@ -61,20 +62,23 @@ void opticalFlow::calculateFlow()
 			drawOptFlowMap(0.5, 16, CV_RGB(0, 255, 0));
 			//imshow("flow", this->cflow);
 			this->drawHsv();
-			this->gray_bgr = cv::Mat::zeros(this->frame.rows, this->frame.cols, CV_8UC1);
+			this->gray_bgr = cv::Mat::zeros(this->images[this->currentFrame].rows, this->images[this->currentFrame].cols, CV_8UC1);
 			cv::cvtColor(this->img_bgr, this->gray_bgr, CV_BGR2GRAY);
 			cv::normalize(this->gray_bgr, this->gray_bgr, 0, 255, cv::NORM_MINMAX, CV_8UC1);
 			cv::blur(this->gray_bgr, this->gray_bgr, cv::Size(5, 5));
 			//imshow("gray", this->gray_bgr);
 
 			this->detectEdges();
+			this->addToJson();
 
 		}
-		//cv::waitKey(5); 
+		cv::waitKey(5); 
 		std::swap(this->prevgray, this->gray);
+		this->currentFrame++;
+		this->boxes.erase(boxes.begin(), boxes.end());
 	}
 	
-
+}
 
 void opticalFlow::drawHsv() {
 	//extract x and y channels
@@ -118,5 +122,39 @@ void opticalFlow::drawOptFlowMap(double scale, int step, const cv::Scalar& color
 			cv::circle(this->cflow, cv::Point(x, y), 2, color, -1);
 		}
 }
+
+void opticalFlow::createJsonObj(const v8::FunctionCallbackInfo<v8::Value> &args)
+{
+	this->isolate = args.GetIsolate();
+	this->obj = v8::Object::New(isolate);
+}
+
+void opticalFlow::addToJson()
+{
+
+	v8::Local<v8::Array> array = v8::Array::New(this->isolate, 4);
+
+	for (int i = 0; i < this->boxes.size(); ++i)
+	{
+		
+		array->Set(0, v8::Integer::New(this->isolate, boxes[i][0]));
+      	array->Set(1, v8::Integer::New(this->isolate, boxes[i][1]));
+      	array->Set(2, v8::Integer::New(this->isolate, boxes[i][2]));
+      	array->Set(3, v8::Integer::New(this->isolate, boxes[i][3]));
+
+		this->obj->Set(v8::String::NewFromUtf8(this->isolate, this->imageNames[currentFrame].c_str()), array);
+	}
+
+}
+
+void opticalFlow::sendJson(const v8::FunctionCallbackInfo<v8::Value> &args)
+{
+	//auto total = v8::Number::New(this->isolate, 2);
+	//args.GetReturnValue().Set(total);
+
+	args.GetReturnValue().Set(this->obj);
+}
+
+
 
 
