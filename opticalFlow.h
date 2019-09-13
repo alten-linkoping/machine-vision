2ff859f187ee2718d6455dc047d4743b4363de61
arturchilangwa <54268901+arturchilangwa@users.noreply.github.com>
2019-09-13 15:33:44 +0200

    Update opticalFlow.h

--- opticalFlow.h
+++ opticalFlow.h
@@ -5,10 +5,10 @@
 #define opticalFlow_H
 
 #include <opencv2/opencv.hpp>
-#include <opencv2/tracking.hpp>
-#include <opencv2/core/ocl.hpp>
-#include "opencv2/highgui.hpp"
-#include "opencv2/imgproc.hpp"
+//#include <opencv2/tracking.hpp>
+//#include <opencv2/core/ocl.hpp>
+//#include "opencv2/highgui.hpp"
+//#include "opencv2/imgproc.hpp"
 //#include <node.h>
 
 class opticalFlow
